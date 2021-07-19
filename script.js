51454b9bba66c883bbbd1b88dc635a693d281308
luponq <neverflex2@gmail.com>
2021-07-19 14:15:30 +0300

    uspeh

--- script.js
+++ script.js
@@ -1,31 +1,56 @@
-let addMessege = document.querySelector('.message'),
-    addButton = document.querySelector('.add'),
-    todo = document.querySelector('.todo');
-
-let todoList = [];
-
-addButton.addEventListener('click', function(){
-    let newTodo = {
-        todo: addMessege.nodeValue,
-        checked: false,
-        important: false
+Vue.component('task',{
+    props: ['data'],
+    data() {
+      return {
+      }
+    },
+    methods: {
+      task_done(){
+        this.$emit('task_done')
+      }
+    },
+    template: `
+    <div class="task">
+      <div>
+        <h3 class="task__title">{{data.title}}</h3>
+        <p v-if="data.desc!=''" class="task__desc">{{data.desc}}</p>
+      </div>
+      <button class="task__done" @click="task_done()">✔️</button>
+    </div>`
+  })
+  
+  var vue = new Vue({
+    el: '#app',
+    data: {
+      string: 'asdasda',
+      new_task: {
+        title: '',
+        desc: ''
+      },
+      tasks : [
+        {
+          title: 'Изучить основы vue.js',
+          desc: 'Попробовать написать калькулятор',
+        },
+        {
+          title: 'Прочитать книгу "Vue.js в действии"',
+          desc: '',
+        }
+      ]
+    },
+    methods: {
+      add_task(){
+        if(this.new_task.title != ''){
+          this.tasks.push({
+            title: this.new_task.title,
+            desc: this.new_task.desc
+          });
+          this.new_task.title='';
+          this.new_task.desc='';
+        }
+      },
+      delete_task(id){
+        this.tasks.splice(id,1);
+      }
     }
-});
-
-todoList.push(newTodo);
-displayMesages();
-
-
-function displayMesages(){
-    let displayMesage = ''
-    todoList.forEach(function(icon, i){
-        displayMesage += `
-        <li>
-            <input type='checkbox' id='item_${i}'>
-            <label for='item_${i}'>${item.todo}</label>    
-        </li>
-        `;
-        todo.innerHTML = displayMesage;
-});
-
-}
+  })
