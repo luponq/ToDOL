dc42bd46cfbf893021a8df46176715665ceaf7c9
luponq <neverflex2@gmail.com>
2021-08-12 11:16:10 +0300

    opisanie

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 Vue.component('task',{
-    props: ['data'],
-    data() {
+    props: ['data'],  //"props элемент который мы будем перебрасывать из головного обьекта вью в component('task')"
+    data() {    //"В обьекте data будут хранится все задания"
       return {
       }
     },
     methods: {
       task_done(){
-        this.$emit('task_done')
+        this.$emit('task_done') //ivent который удаляет задачу из массива tasks
       }
-    },
-    template: `
+    },               //template-"верстка самого компонента / фукционал"
+    template: `     
     <div class="task">
       <div>
         <h3 class="task__title">{{data.title}}</h3>
@@ -18,8 +18,8 @@ Vue.component('task',{
       <button class="task__done" @click="task_done()">✔️</button>
     </div>`
   })
-  
-  var vue = new Vue({
+  // Строка 18 выполняет функционал выполнение/удаление заадачи
+  var vue = new Vue({ //"Задачи и их описание которые отображаются по умолчнию"
     el: '#app',
     data: {
       string: 'asdasda',
@@ -33,23 +33,23 @@ Vue.component('task',{
           desc: 'Попробовать написать калькулятор',
         },
         {
-          title: 'Прочитать книгу "Vue.js в действии"',
+          title: 'Прочитать книгу "Самый богатый человек в вавилоне"',
           desc: '',
         }
       ]
     },
-    methods: {
-      add_task(){
-        if(this.new_task.title != ''){
-          this.tasks.push({
+    methods: {      
+      add_task(){      //Формирует обьект после которого он добавится в массив tasks
+        if(this.new_task.title != ''){    //Если строка заполнена,то выполняется всё что внутри фигурных скобок
+          this.tasks.push({       //Функция push добавляет в конец массива добавляет обьект
             title: this.new_task.title,
             desc: this.new_task.desc
           });
-          this.new_task.title='';
+          this.new_task.title=''; //Очистка полей для ввода
           this.new_task.desc='';
         }
       },
-      delete_task(id){
+      delete_task(id){           //
         this.tasks.splice(id,1);
       }
     }
